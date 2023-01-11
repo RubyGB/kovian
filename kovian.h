8a693aab1a97fb221f3d3c8f357e3033a7763192
RubyGB <rubyGB@protonmail.com>
2023-01-11 16:15:57 -0500

    Implemented file reading of comma/space separated transition matrices. Format is N lines of N doubles.

--- kovian.h
+++ kovian.h
@@ -8,10 +8,36 @@
 #include <random>     // for generating discrete random variables
 #include <chrono>     // for seeding using system clock
 #include <functional> // for std::function
+#include <fstream>    // for file IO
+#include <regex>      // for file IO help
 
 namespace kovian {
 	template<size_t N>
 	using TransitionMatrix = std::array<double, N*N>;
+	class TransitionMatrixParseError : public std::runtime_error {
+		public: TransitionMatrixParseError(const std::string &what) : std::runtime_error("Error parsing transition matrix file: " + what) {}
+	};
+	
+	// Helper method which builds transition matrix from file (double values comma or space separated, N on each line, N lines).
+	template<size_t N>
+	TransitionMatrix<N> transitionMatrixFromFile(const std::string &matrixFile) {
+		std::ifstream mfile(matrixFile);
+		TransitionMatrix<N> result;
+		std::string line;
+		std::regex csreg {R"([\s,]+)"};
+		size_t k = 0;
+		std::vector<std::string> row;
+		while(std::getline(mfile, line)) {
+			if(k >= N) throw TransitionMatrixParseError("Number of rows exceeds matrix dimension N=" + std::to_string(N));
+			std::sregex_token_iterator it {line.begin(), line.end(), csreg, -1};
+			row = {it, {}};
+			if(row.size() != N) throw TransitionMatrixParseError("Row " + std::to_string(k) + " does not contain N=" + std::to_string(N) + " entries.");
+			for(size_t i = 0; i < N; ++i)
+				result[i + k*N] = std::stod(row[i]);
+			++k;
+		}
+		return result;
+	}
 	
 	// Defines a Markov chain with N nodes indexed 0, 1, ... N-1.
 	template<size_t N>
@@ -25,6 +51,7 @@ namespace kovian {
 			for(size_t k = 0; k < N; ++k)
 				onestep[k] = std::discrete_distribution<size_t>(P.begin() + k*N, P.begin() + (k+1)*N);
 		}
+		MarkovChain(const std::string &matrixFile) : MarkovChain(transitionMatrixFromFile<N>(matrixFile)) {}
 		
 		// Advances the Markov chain by one step and returns the new state.
 		size_t advance() { return state = onestep[state](rng); }
@@ -47,6 +74,11 @@ namespace kovian {
 	
 	template<typename T>
 	using CostFunction = std::function<double(const T&)>;
+	
+	namespace aliases {
+		template<size_t N>
+		using TM = TransitionMatrix<N>;
+	}
 }
 
 #endif
--- sample.cpp
+++ sample.cpp
@@ -2,15 +2,19 @@
 #include "kovian.h"
 
 int main() {
-	kovian::MarkovChain<5> mc({
+	using namespace kovian::aliases;
+	kovian::MarkovChain<5> mc("sample1.kov");
+	/*kovian::MarkovChain<5> mc(TM<5>{
 		0.1, 0.3, 0.4, 0.0, 0.2,
 		0.0, 0.0, 0.0, 0.0, 1.0,
 		0.0, 0.5, 0.5, 0.0, 0.0,
 		0.0, 0.1, 0.1, 0.1, 0.1,
-		0.1, 0.1, 0.1, 0.1, 0.1});
-	mc.setState(3); std::cout << "3 -> ";
+		0.1, 0.1, 0.1, 0.1, 0.1});*/
+	mc.setState(3);
+	std::cout << "3 -> ";
 	while(mc.advance() != 0) {
 		std::cout << mc.current() << " -> ";
-	} std::cout << "0" << std::endl;
+	}
+	std::cout << "0" << std::endl;
 	return 0;
 }
